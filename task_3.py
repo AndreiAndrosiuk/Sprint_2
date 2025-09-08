9664c28a6ab6e96c53662d9700b368bea91142b1
Andrej Safronov <podrivdance@gmail.com>
2025-09-08 23:31:27 +0300

    Исправлены замечания для Задачи 3

--- task_3.py
+++ task_3.py
@@ -3,10 +3,12 @@ class PointsForPlace:
     def get_points_for_place(place):
         if place > 100:
             print('Баллы начисляются только первым 100 участникам.')
-
+            return None
+        
         elif place < 1:
             print('Спортсмен не может занять нулевое или отрицательное место.')
-            
+            return None
+        
         else:
             points = 101 - place
             return points
@@ -16,6 +18,7 @@ class PointsForMeters:
     def get_points_for_meters(meters):
         if meters < 0:
             print('Количество метров не может быть отрицательным.')
+            return None
         
         else:
             points = meters * 0.5
@@ -23,12 +26,15 @@ class PointsForMeters:
 
 class TotalPoints(PointsForPlace, PointsForMeters):
     def get_total_points(self, meters, place):
-        points_from_place = PointsForPlace.get_points_for_place(place)
-        points_from_meters = PointsForMeters.get_points_for_meters(meters)
+        points_from_place = self.get_points_for_place(place)
+        points_from_meters = self.get_points_for_meters(meters)
         
-        total = points_from_place + points_from_meters
-        return total
-
+        if points_from_place is not None and points_from_meters is not None:
+            total = points_from_place + points_from_meters
+            return total
+        else:
+            return None
+        
 points_for_place = PointsForPlace()
 print(points_for_place.get_points_for_place(10))
 
