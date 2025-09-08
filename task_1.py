9365055ac1332b817387e7f85c7a7895f0ef1fdc
Andrej Safronov <podrivdance@gmail.com>
2025-09-08 23:06:07 +0300

    Исправлены замеания для Задания 1

--- task_1.py
+++ task_1.py
@@ -18,11 +18,8 @@ class ExtendedCase(Case): # напиши свой код здесь
         super().__init__(test_case_id, name, step_description, expected_result)
 
     def print_test_case_info(self):
-        print(f"ID тест-кейса: {self.test_case_id}"
-              f"\nНазвание: {self.name}"
-              f"\nОписание шага: {self.step_description}"
-              f"\nОжидаемый результат: {self.expected_result}"
-              f"\nПредусловие: {self.precondition}"
+        super().print_test_case_info()
+        print(f"\nПредусловие: {self.precondition}"
               f"\nОкружение: {self.enviroment}") 
         
 case = ExtendedCase("1", "Наличие кнопки принять", "1. Открыть вкладку приёма документов 2. Проверить наличие кнопки ",
