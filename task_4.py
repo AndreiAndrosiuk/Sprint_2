93325f79d35df08b5d5067ef297188cba794120f
Andrej Safronov <podrivdance@gmail.com>
2025-09-09 00:10:03 +0300

    Исправлены замечания для Задания 4

--- task_4.py
+++ task_4.py
@@ -7,17 +7,21 @@ class Employee:
         self.rest_days = rest_days
         self.email = email
 
-    def get_hours(self):
-        if self.hours:
-            return self.hours
-        else:
-            return (7 - self.rest_days) * 8
+    @classmethod
+    def get_hours(cls, name, hours, rest_days, email):
+        if hours is None:
+            hours = (7 - rest_days) * 8
+        return cls(name, hours, rest_days, email)
 
-    def get_email(self):
-        if self.email:
-            return self.email
-        else:
-            return f"{self.name}@email.com"
+    @classmethod
+    def get_email(cls, name, hours, rest_days, email):
+        if email is None:
+            email = f"{name}@email.com"
+        return cls(name, hours, rest_days, email)
+
+    @classmethod    
+    def set_hourly_payment(cls, new_payment):
+        cls.hourly_payment = new_payment
 
     def salary(self):
-        return self.get_hours() * self.__class__.hourly_payment
+        return self.hours * self.hourly_payment
